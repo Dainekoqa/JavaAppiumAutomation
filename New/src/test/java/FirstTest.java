5870fd4760b676544f23a86f6498ec95b4c096b3
Dainekoqa <dainekoqa@gmail.com>
2025-05-04 23:15:09 +0300

    Ex2–Ex4: Add Wikipedia search tests

--- New/src/test/java/FirstTest.java
+++ New/src/test/java/FirstTest.java
@@ -4,12 +4,21 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class FirstTest {
 
     private AppiumDriver driver;
+    private WebDriverWait wait;
 
     @Before
     public void setUp() throws Exception {
@@ -24,6 +33,7 @@ public class FirstTest {
                 .setAppActivity("org.wikipedia.main.MainActivity");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @After
@@ -36,5 +46,80 @@ public class FirstTest {
     @Test
     public void firstTest() {
         System.out.println("App launched successfully!");
+    }
+
+    @Test
+    public void testSearchFieldText() {
+        WebElement skipButton = wait.until(
+                ExpectedConditions.elementToBeClickable(By.id("org.wikipedia:id/fragment_onboarding_skip_button"))
+        );
+        skipButton.click();
+
+        WebElement searchFieldContainer = wait.until(
+                ExpectedConditions.presenceOfElementLocated(By.id("org.wikipedia:id/search_container"))
+        );
+
+        WebElement searchText = searchFieldContainer.findElement(By.className("android.widget.TextView"));
+        String actualText = searchText.getText();
+
+        assertEquals("Search Wikipedia", actualText);
+    }
+
+    @Test
+    public void testSearchCancel() {
+        WebElement skipButton = wait.until(
+                ExpectedConditions.elementToBeClickable(By.id("org.wikipedia:id/fragment_onboarding_skip_button"))
+        );
+        skipButton.click();
+
+        WebElement searchField = wait.until(
+                ExpectedConditions.elementToBeClickable(By.id("org.wikipedia:id/search_container"))
+        );
+        searchField.click();
+
+        WebElement input = wait.until(
+                ExpectedConditions.presenceOfElementLocated(By.id("org.wikipedia:id/search_src_text"))
+        );
+        input.sendKeys("Java");
+
+        List<WebElement> results = wait.until(
+                ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("org.wikipedia:id/page_list_item_title"))
+        );
+        assertTrue("Expected more than one result", results.size() > 1);
+
+        WebElement cancel = driver.findElement(By.id("org.wikipedia:id/search_close_btn"));
+        cancel.click();
+
+        List<WebElement> clearedResults = driver.findElements(By.id("org.wikipedia:id/page_list_item_title"));
+        assertEquals("Search results should be cleared after cancel", 0, clearedResults.size());
+    }
+
+    @Test
+    public void testSearchResultsContainWord() {
+        WebElement skipButton = wait.until(
+                ExpectedConditions.elementToBeClickable(By.id("org.wikipedia:id/fragment_onboarding_skip_button"))
+        );
+        skipButton.click();
+
+        WebElement searchField = wait.until(
+                ExpectedConditions.elementToBeClickable(By.id("org.wikipedia:id/search_container"))
+        );
+        searchField.click();
+
+        WebElement input = wait.until(
+                ExpectedConditions.presenceOfElementLocated(By.id("org.wikipedia:id/search_src_text"))
+        );
+        input.sendKeys("Java");
+
+        List<WebElement> results = wait.until(
+                ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("org.wikipedia:id/page_list_item_title"))
+        );
+
+        assertFalse("No search results found", results.isEmpty());
+
+        for (WebElement result : results) {
+            String text = result.getText().toLowerCase();
+            assertTrue("Search result does not contain 'java': " + text, text.contains("java"));
+        }
     }
 }
